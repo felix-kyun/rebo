b49f4859dbda3f43da2e1c68c1386663f57aae4d
Praise Jacob <iamfelixkyun@gmail.com>
2025-05-23 23:19:56 +0530

    add fallback to use best format in case download with specific resolution fails

--- utils/downloadVideo.mjs
+++ utils/downloadVideo.mjs
@@ -1,11 +1,12 @@
 import { fileExists } from "./fileExists.mjs";
 import { exec } from "child_process";
 import { promisify } from "util";
+import { v4 as uuidv4 } from "uuid";
 
 const execPromise = promisify(exec);
 
 export const downloadVideo = async (url) => {
-  const fileName = url.split("/").pop();
+  const fileName = uuidv4();
   const output = `./.downloadCache/output/${fileName}.mp4`;
 
   // if file is already downloaded, return
@@ -15,9 +16,15 @@ export const downloadVideo = async (url) => {
 
   try {
     // download the video using yt-dlp
-    await execPromise(
-      `/usr/bin/yt-dlp -S filesize --merge-output-format mp4 -f 'bv[height=720]+ba/b[height=720]/bv[height=480]+ba/b[height=480]/bv[height=1080]+ba/b[height=1080]' -o './.downloadCache/${fileName}.%(ext)s' ${url}`,
-    );
+    try {
+      await execPromise(
+        `/usr/bin/yt-dlp -S filesize --merge-output-format mp4 -f 'bv[height=720]+ba/b[height=720]/bv[height=480]+ba/b[height=480]/bv[height=1080]+ba/b[height=1080]/bv[height=1024]+ba/b[height=1024]' -o './.downloadCache/${fileName}.%(ext)s' ${url}`,
+      );
+    } catch (err) {
+      await execPromise(
+        `/usr/bin/yt-dlp -S filesize --merge-output-format mp4  -o './.downloadCache/${fileName}.%(ext)s' ${url}`,
+      );
+    }
 
     // fix the codec as required by whatsapp
     await execPromise(
