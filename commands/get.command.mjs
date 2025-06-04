12d050fddc1f6fa5f6e251623c5982ce9ed652c4
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-04 17:20:50 +0530

    add auto user creationg and attach user to message along with commands to set and get preferences

--- commands/get.command.mjs
+++ commands/get.command.mjs
@@ -1,12 +1,14 @@
 import { Chat } from "../models/chat.model.mjs";
 
 export async function getHandler(message) {
+    const { user } = message;
     const [key] = message.args;
+
     if (!key) throw new Error("Key must be provided");
 
-    const user = await Chat.findOne({ chatId: message.from });
-    if (!user) throw new Error("User not found");
+    if (!(key in user.preferences)) {
+        throw new Error(`Preference key "${key}" does not exist`);
+    } else message.reply(`*${key}:* _${user.preferences[key]}_`);
 
-    message.reply(`*${key}: ${user.preferences[key] || "undefined"}*`);
     return true;
 }
--- commands/set.command.mjs
+++ commands/set.command.mjs
@@ -2,10 +2,8 @@ import { Chat } from "../models/chat.model.mjs";
 
 export async function setHandler(message) {
     const [key, value] = message.args;
+    const { from, user } = message;
     if (!key || !value) throw new Error("Key and value must be provided");
-
-    const user = await Chat.findOne({ chatId: message.from });
-    if (!user) throw new Error("User not found");
 
     user.preferences = { ...user.preferences, [key]: value };
     await user.save();
